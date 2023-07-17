cc7e68904d9b28f79dd56359c53fc109cb41de0e
the-void-century <utsdhall@gmail.com>
2023-07-17 17:50:48 +0530

    Added has_many relationships with registered model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
     has_one_attached :eventposter
+    has_many :registereds
 end
--- app/models/profileuser.rb
+++ app/models/profileuser.rb
@@ -1,3 +1,4 @@
 class Profileuser < ApplicationRecord
   has_one_attached :profilepicture
+  has_many :registereds
 end
